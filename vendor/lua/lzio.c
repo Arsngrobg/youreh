abceddf0c334dd5d42ea9a71b84de5e668181716
James Armstrong <arsngrobg@gmail.com>
2026-07-24 04:37:32 +0100

    fix(lua): include lua 5.1

--- vendor/lua/lzio.c
+++ vendor/lua/lzio.c
@@ -1,20 +1,17 @@
 /*
-** $Id: lzio.c $
-** Buffered streams
+** $Id: lzio.c,v 1.31.1.1 2007/12/27 13:02:25 roberto Exp $
+** a generic input stream interface
 ** See Copyright Notice in lua.h
 */
-
-#define lzio_c
-#define LUA_CORE
-
-#include "lprefix.h"
 
 
 #include <string.h>
 
+#define lzio_c
+#define LUA_CORE
+
 #include "lua.h"
 
-#include "lapi.h"
 #include "llimits.h"
 #include "lmem.h"
 #include "lstate.h"
@@ -28,11 +25,23 @@ int luaZ_fill (ZIO *z) {
   lua_unlock(L);
   buff = z->reader(L, z->data, &size);
   lua_lock(L);
-  if (buff == NULL || size == 0)
-    return EOZ;
-  z->n = size - 1;  /* discount char being returned */
+  if (buff == NULL || size == 0) return EOZ;
+  z->n = size - 1;
   z->p = buff;
-  return cast_uchar(*(z->p++));
+  return char2int(*(z->p++));
+}
+
+
+int luaZ_lookahead (ZIO *z) {
+  if (z->n == 0) {
+    if (luaZ_fill(z) == EOZ)
+      return EOZ;
+    else {
+      z->n++;  /* luaZ_fill removed first byte; put back it */
+      z->p--;
+    }
+  }
+  return char2int(*z->p);
 }
 
 
@@ -46,25 +55,11 @@ void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader, void *data) {
 
 
 /* --------------------------------------------------------------- read --- */
-
-static int checkbuffer (ZIO *z) {
-  if (z->n == 0) {  /* no bytes in buffer? */
-    if (luaZ_fill(z) == EOZ)  /* try to read more */
-      return 0;  /* no more input */
-    else {
-      z->n++;  /* luaZ_fill consumed first byte; put it back */
-      z->p--;
-    }
-  }
-  return 1;  /* now buffer has something */
-}
-
-
 size_t luaZ_read (ZIO *z, void *b, size_t n) {
   while (n) {
     size_t m;
-    if (!checkbuffer(z))
-      return n;  /* no more input; return number of missing bytes */
+    if (luaZ_lookahead(z) == EOZ)
+      return n;  /* return number of missing bytes */
     m = (n <= z->n) ? n : z->n;  /* min. between n and z->n */
     memcpy(b, z->p, m);
     z->n -= m;
@@ -75,15 +70,13 @@ size_t luaZ_read (ZIO *z, void *b, size_t n) {
   return 0;
 }
 
-
-const void *luaZ_getaddr (ZIO* z, size_t n) {
-  const void *res;
-  if (!checkbuffer(z))
-    return NULL;  /* no more input */
-  if (z->n < n)  /* not enough bytes? */
-    return NULL;  /* block not whole; cannot give an address */
-  res = z->p;  /* get block address */
-  z->n -= n;  /* consume these bytes */
-  z->p += n;
-  return res;
+/* ------------------------------------------------------------------------ */
+char *luaZ_openspace (lua_State *L, Mbuffer *buff, size_t n) {
+  if (n > buff->buffsize) {
+    if (n < LUA_MINBUFFER) n = LUA_MINBUFFER;
+    luaZ_resizebuffer(L, buff, n);
+  }
+  return buff->buffer;
 }
+
+
